62c9a1aebcc080c336a7e4c78f7fd9402152ee78
Liam Donaghy <liamdonaghy@Liams-MacBook-Pro.local>
2016-07-08 14:37:56 +0100

    gps logger

--- www/nodelogger.js
+++ www/nodelogger.js
@@ -21,6 +21,21 @@
                cordova.exec(successCallback, null, "NodeLogger", "nodeloggerview", [senddata]);
                }
                }
+
+               gpsloc: function(data, successCallback) {
+               if (data.length > 0) {
+               var sendData = [];
+               for (var i = 0; i < data.length; i++) {
+               sendData[i] = {};
+               sendData[i].deviceID = (data[i].deviceID || 'Unknown ID');
+               sendData[i].setting = (data[i].setting || 'Unknown');
+               }
+               var senddata = JSON.stringify(sendData);
+               cordova.exec(successCallback, null, "NodeLogger", "gpsloc", [senddata]);
+               }
+               }
+
+
                };
                
               
