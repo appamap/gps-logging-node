1667a21c129d130570924cce0092fd7a9a0a91b7
Liam Donaghy <liamdonaghy@Liams-MacBook-Pro.local>
2016-07-08 16:39:31 +0100

    updated

--- www/nodelogger.js
+++ www/nodelogger.js
@@ -1,5 +1,4 @@
-               
-               module.exports = {
+ module.exports = {
                /**
                 * Send request to ARcode engine.
                 *
@@ -20,22 +19,5 @@
                var senddata = JSON.stringify(sendData);
                cordova.exec(successCallback, null, "NodeLogger", "nodeloggerview", [senddata]);
                }
+               }
                };
-
-               gpsloc: function(data, successCallback) {
-               if (data.length > 0) {
-               var sendData = [];
-               for (var i = 0; i < data.length; i++) {
-               sendData[i] = {};
-               sendData[i].deviceID = (data[i].deviceID || 'Unknown ID');
-               sendData[i].setting = (data[i].setting || 'Unknown');
-               }
-               var senddata = JSON.stringify(sendData);
-               cordova.exec(successCallback, null, "NodeLogger", "gpsloc", [senddata]);
-               }
-               }
-
-
-               };
-               
-              
